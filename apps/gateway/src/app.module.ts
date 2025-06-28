da9b75dae28f6e8743e236859c1e92bd285126f4
amoljagtap17 <amoljagtap17@gmail.com>
2025-06-28 20:45:07 +0530

    graphql-federation-idea: add graphql config to gateway

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -1,10 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { IntrospectAndCompose } from "@apollo/gateway";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from "@nestjs/apollo";
+import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      server: {
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        includeStacktraceInErrorResponses: false,
+        playground: false,
+      },
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [{ name: "classsification", url: "http://localhost:3001" }],
+          subgraphHealthCheck: true,
+        }),
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

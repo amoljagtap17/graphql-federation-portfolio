a5b43790fe492f29d6431466fc6fbc561ed24704
amoljagtap17 <amoljagtap17@gmail.com>
2025-06-29 11:08:45 +0530

    feature/docker: add graphql config to assset-classification-subgraph

--- apps/asset-classification-subgraph/src/asset-classification-subgraph.module.ts
+++ apps/asset-classification-subgraph/src/asset-classification-subgraph.module.ts
@@ -1,8 +1,25 @@
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from "path";
 import { BroadAssetClassModule } from "./broad-asset-class/broad-asset-class.module";
 
 @Module({
-	imports: [BroadAssetClassModule],
+	imports: [
+		GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+			driver: ApolloFederationDriver,
+			autoSchemaFile: {
+				federation: 2,
+				path: join(process.cwd(), "apps/asset-classification-subgraph", "src/schema.gql"),
+			},
+			playground: false,
+			sortSchema: true,
+			graphiql: true,
+			plugins: [ApolloServerPluginLandingPageLocalDefault()],
+		}),
+		BroadAssetClassModule,
+	],
 	controllers: [],
 	providers: [],
 })

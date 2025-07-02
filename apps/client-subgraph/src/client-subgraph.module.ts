a7a1b4d181e5899784d4ab589a0dfdb70d75242b
amoljagtap17 <amoljagtap17@gmail.com>
2025-07-02 10:16:02 +0530

    add client and account modules to client subgraph

--- apps/client-subgraph/src/client-subgraph.module.ts
+++ apps/client-subgraph/src/client-subgraph.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientSubgraphController } from './client-subgraph.controller';
 import { ClientSubgraphService } from './client-subgraph.service';
+import { ClientModule } from './client/client.module';
+import { AccountModule } from './account/account.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, AccountModule],
   controllers: [ClientSubgraphController],
   providers: [ClientSubgraphService],
 })

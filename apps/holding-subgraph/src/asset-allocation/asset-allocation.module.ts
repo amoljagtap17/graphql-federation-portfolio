b81ea9aa1863938025f69b30ac114967998ef900
amoljagtap17 <amoljagtap17@gmail.com>
2025-07-05 16:04:39 +0530

    add database module to asset allocation module

--- apps/holding-subgraph/src/asset-allocation/asset-allocation.module.ts
+++ apps/holding-subgraph/src/asset-allocation/asset-allocation.module.ts
@@ -1,8 +1,11 @@
+import { DatabaseModule } from "@app/common";
 import { Module } from "@nestjs/common";
 import { AssetAllocationResolver } from "./asset-allocation.resolver";
 import { AssetAllocationService } from "./asset-allocation.service";
+import { AssetAllocation } from "./entities/asset-allocation.entity";
 
 @Module({
+	imports: [DatabaseModule, DatabaseModule.forFeature([AssetAllocation])],
 	providers: [AssetAllocationResolver, AssetAllocationService],
 	exports: [AssetAllocationService],
 })

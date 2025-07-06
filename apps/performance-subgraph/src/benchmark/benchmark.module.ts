cc6ae7744eb9a7d9c62db797d5d0ba1ac367e0a1
amoljagtap17 <amoljagtap17@gmail.com>
2025-07-06 10:38:43 +0530

    add database module to benchmark

--- apps/performance-subgraph/src/benchmark/benchmark.module.ts
+++ apps/performance-subgraph/src/benchmark/benchmark.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { BenchmarkService } from './benchmark.service';
-import { BenchmarkResolver } from './benchmark.resolver';
+import { DatabaseModule } from "@app/common";
+import { Module } from "@nestjs/common";
+import { BenchmarkResolver } from "./benchmark.resolver";
+import { BenchmarkService } from "./benchmark.service";
+import { Benchmark } from "./entities/benchmark.entity";
 
 @Module({
-  providers: [BenchmarkResolver, BenchmarkService],
+	imports: [DatabaseModule, DatabaseModule.forFeature([Benchmark])],
+	providers: [BenchmarkResolver, BenchmarkService],
 })
 export class BenchmarkModule {}
